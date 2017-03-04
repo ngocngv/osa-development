890053dd1b9f512a8bf274493a15d293521c22d5
root <root@infra01.openstack.local>
2017-03-04 15:20:31 +0000

    push to execute post-receive

---
+++ haproxy/lxc.py
@@ -0,0 +1,28 @@
+NAME                                           STATE   AUTOSTART GROUPS            IPV4                                     IPV6 
+infra01_cinder_api_container-c8ff90ba          RUNNING 1         onboot, openstack 10.0.3.37, 172.29.237.27, 172.29.245.67  -    
+infra01_cinder_scheduler_container-36c7b43d    RUNNING 1         onboot, openstack 10.0.3.85, 172.29.237.131                -    
+infra01_galera_container-8fa6f3c7              RUNNING 1         onboot, openstack 10.0.3.86, 172.29.239.10                 -    
+infra01_galera_container-a59d4db4              RUNNING 1         onboot, openstack 10.0.3.166, 172.29.238.137               -    
+infra01_galera_container-ac64dce6              RUNNING 1         onboot, openstack 10.0.3.9, 172.29.237.153                 -    
+infra01_glance_container-633dcaf3              RUNNING 1         onboot, openstack 10.0.3.83, 172.29.238.80, 172.29.247.217 -    
+infra01_heat_apis_container-751280bb           RUNNING 1         onboot, openstack 10.0.3.167, 172.29.239.26                -    
+infra01_heat_engine_container-43ca2a2b         RUNNING 1         onboot, openstack 10.0.3.120, 172.29.238.233               -    
+infra01_horizon_container-2f2e78e0             RUNNING 1         onboot, openstack 10.0.3.180, 172.29.239.162               -    
+infra01_horizon_container-cd28f52b             RUNNING 1         onboot, openstack 10.0.3.135, 172.29.238.41                -    
+infra01_keystone_container-1234be07            RUNNING 1         onboot, openstack 10.0.3.237, 172.29.239.225               -    
+infra01_keystone_container-f80cadb9            RUNNING 1         onboot, openstack 10.0.3.99, 172.29.237.159                -    
+infra01_memcached_container-ab85877b           RUNNING 1         onboot, openstack 10.0.3.218, 172.29.237.239               -    
+infra01_neutron_agents_container-a7169944      RUNNING 1         onboot, openstack 10.0.3.79, 172.29.238.51, 172.29.242.80  -    
+infra01_neutron_server_container-53699e6c      RUNNING 1         onboot, openstack 10.0.3.64, 172.29.239.77                 -    
+infra01_nova_api_metadata_container-4a25ab60   RUNNING 1         onboot, openstack 10.0.3.199, 172.29.239.176               -    
+infra01_nova_api_os_compute_container-d01caeb9 RUNNING 1         onboot, openstack 10.0.3.210, 172.29.237.109               -    
+infra01_nova_cert_container-6cdf6957           RUNNING 1         onboot, openstack 10.0.3.223, 172.29.239.180               -    
+infra01_nova_conductor_container-203bdfd6      RUNNING 1         onboot, openstack 10.0.3.181, 172.29.237.228               -    
+infra01_nova_console_container-c5d1b787        RUNNING 1         onboot, openstack 10.0.3.160, 172.29.239.174               -    
+infra01_nova_scheduler_container-6f81482f      RUNNING 1         onboot, openstack 10.0.3.231, 172.29.239.244               -    
+infra01_rabbit_mq_container-549ca760           RUNNING 1         onboot, openstack 10.0.3.250, 172.29.237.83                -    
+infra01_rabbit_mq_container-a2dc4d5e           RUNNING 1         onboot, openstack 10.0.3.94, 172.29.237.233                -    
+infra01_rabbit_mq_container-f18fa62f           RUNNING 1         onboot, openstack 10.0.3.157, 172.29.238.208               -    
+infra01_repo_container-e4111261                RUNNING 1         onboot, openstack 10.0.3.89, 172.29.239.187                -    
+infra01_rsyslog_container-f92a6876             RUNNING 1         onboot, openstack 10.0.3.214, 172.29.237.128               -    
+infra01_utility_container-c06eaee7             RUNNING 1         onboot, openstack 10.0.3.61, 172.29.238.70                 -    
